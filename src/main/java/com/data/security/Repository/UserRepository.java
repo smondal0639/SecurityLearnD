e3409557a53a463b75b6ed38906146d1f3b55a3a
supriyo <er.supriya93@gmail.com>
2022-09-09 23:08:30 +0530

    added security

--- src/main/java/com/data/security/Repository/UserRepository.java
+++ src/main/java/com/data/security/Repository/UserRepository.java
@@ -4,7 +4,10 @@ import com.data.security.Entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<Users,Integer> {
 
+    Optional<Users> findByusername(String username);
 }
