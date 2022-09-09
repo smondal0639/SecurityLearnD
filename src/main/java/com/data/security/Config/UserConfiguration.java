95c97d692577eabbfd43561aee307eb29f4f3822
supriyo <er.supriya93@gmail.com>
2022-09-09 21:03:01 +0530

    added WebSecutiry

--- src/main/java/com/data/security/Config/UserConfiguration.java
+++ src/main/java/com/data/security/Config/UserConfiguration.java
@@ -1,17 +1,28 @@
 package com.data.security.Config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class UserConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .authorizeRequests()
+                .antMatchers("/api/v1/users/addnew").permitAll()
+                .anyRequest().authenticated()
+                .and()
                 .csrf().disable();
     }
 
@@ -19,4 +30,5 @@ public class UserConfiguration extends WebSecurityConfigurerAdapter {
     public BCryptPasswordEncoder getPassEncoder(){
         return new BCryptPasswordEncoder();
     }
+
 }
